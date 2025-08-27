8ddb58d30a2674c9357568aae45db95b529deae9
KeeDan204 <danikaymonique@gmail.com>
2025-08-27 15:15:06 -0500

    updated all files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import Weather from './Weather';
 
+import './App.css';
+import Search from "./Search.js"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <h1>Hello React</h1>
-        <Weather />
-     
-      </header>
+      
+      <h1>Weather App</h1>
+      <Search/>
     </div>
   );
 }
